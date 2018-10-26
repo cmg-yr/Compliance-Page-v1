8f6f5fe02e6cf319bfc1853dd3452966d520f401
Greg <greg@guavatek.tech>
2018-10-26 15:42:03 -0700

    second commit

--- v4/mobile/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).scroll(function() {
-    var anchor = $('#section5').offset().top;
-    var y = $(this).scrollTop();
-    if (y > anchor){
-        $('.legal-footer').slideDown("fast");
-    } else {
-        $('.legal-footer').slideUp("fast");
-    }
-});
